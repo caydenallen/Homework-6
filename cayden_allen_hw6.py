6250fc77b837a63fa423aa6d8ac8bee202a5a6bb
Cayden Allen <caydenallen@mail.weber.edu>
2017-03-22 17:44:52 -0600

    basic template

---
+++ cayden_allen_hw6.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python3
+import sys
+from urllib.request import urlopen
+
+def ():
+"""
+
+"""
+
+
+def main():
+    # test function
+    
+
+if __name__ == "__main__":
+    main()
+    exit(0)
