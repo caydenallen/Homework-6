6dd4d186fe81908f7f3404839a14497adbd07ade
Cayden Allen <caydenallen@mail.weber.edu>
2017-03-22 18:59:42 -0600

    added the basics

--- cayden_allen_hw6.py
+++ cayden_allen_hw6.py
@@ -2,16 +2,45 @@
 import sys
 from urllib.request import urlopen
 
-def ():
-"""
+url = sys.argv[1]
 
-"""
+def help():
+    """
+    Help Function
+    """
+
+    print("Usage is: ./cayden_allen_hw6 <file Input>")
+
+
+def verify(url):
+    """
+    get_url info
+    """
+
+    if len(sys.argv) != 2:
+        help()
+    
+        
+        
+
+def retrieve():
+    """
+    retrieving url
+    """
+
+    with urlopen(url) as log:
+        print(url)
+        print(log)
 
 
 def main():
     # test function
+      # help()
+   # url = sys.argv[1:]
     
+    verify(url)
+    retrieve()  
 
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
     exit(0)
